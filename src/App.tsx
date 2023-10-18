2191413ac697f2a1cc1f7ea28195135c7a4bdded
frankirova <franciscor1905@gmail.com>
2023-10-18 13:28:50 -0300

    change fetch in getqr function

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,14 @@ function App() {
     try {
       const encodedLink = encodeURIComponent(link);
       // const response = await fetch(`http://localhost:8000/api/qr/${encodedLink}`);
-      const response = await fetch(`https://main--glittery-salamander-4076bc.netlify.app/api/qr/${encodedLink}`);
+      // const response = await fetch(`https://main--glittery-salamander-4076bc.netlify.app/api/qr/${encodedLink}`);
+      const response = await fetch(`https://main--glittery-salamander-4076bc.netlify.app/api/qr/${encodedLink}`, {
+        headers: {
+          'Access-Control-Allow-Origin': 'https://example.com',
+          'Access-Control-Allow-Credentials': 'true',
+        }
+      })
+
 
       const blob = await response.blob();
 
